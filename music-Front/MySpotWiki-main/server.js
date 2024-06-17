0fc019d6b1f5e37e44d11aa38a02267257e230d7
Beatriz <103044603+beabritoliveira@users.noreply.github.com>
2024-06-16 21:17:32 -0300

    Delete music-Front directory

--- music-Front/MySpotWiki-main/server.js
+++
@@ -1,6 +0,0 @@
-// Import the framework and instantiate it
-import { build, options } from './app.js';
-
-  const server = await build(options);
-  await server.listen({port: options.port, host: options.host});
-  
